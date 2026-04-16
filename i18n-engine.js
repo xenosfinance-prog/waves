77de967dfcb5ceeb7fdab09c8f1c67fbce1a0ee3
XenosManolis <xenosfinance@gmail.com>
2026-04-16 18:11:40 +0300

    Add files via upload

--- i18n-engine.js
+++ i18n-engine.js
@@ -4,7 +4,6 @@
 (function() {
   const LANGS = {
     en: { flag: '🇬🇧', label: 'EN' },
-    it: { flag: '🇮🇹', label: 'IT' },
     ru: { flag: '🇷🇺', label: 'RU' },
     pl: { flag: '🇵🇱', label: 'PL' },
     es: { flag: '🇪🇸', label: 'ES' },
